55c47984c2437151e61693fe903e1f64ad585fb9
Alexander Mayers <a.mayers102@gmail.com>
2026-05-22 07:36:08 +0200

    feat(vaultwarden): Enable storing of SSH keys

--- services/vaultwarden.nix
+++ services/vaultwarden.nix
@@ -9,10 +9,12 @@
     dbBackend = "postgresql";
     environmentFile = config.sops.secrets."vaultwarden/env".path;
     config = {
+      # https://github.com/dani-garcia/vaultwarden/blob/1.36.0/.env.template
       DOMAIN = "https://vaultwarden.alexmayers.co.za";
       ROCKET_PORT = 8222;
       ROCKET_ADDRESS = "0.0.0.0";
       SIGNUPS_ALLOWED = false;
+      EXPERIMENTAL_CLIENT_FEATURE_FLAGS = "ssh-key-vault-item,ssh-agent";
     };
   };
 }
