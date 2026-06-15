fc9a42887186a0ade095186392f75491b6e53776
Alexander Mayers <a.mayers102@gmail.com>
2026-06-15 08:42:50 +0200

    chore(observability): Clean up logging

--- config/observability.nix
+++ config/observability.nix
@@ -5,7 +5,6 @@
   ...
 }:
 {
-
   environment.systemPackages = [ pkgs.iperf3 ];
   services.iperf3 = {
     enable = true;
@@ -95,10 +94,41 @@
     }
 
     loki.source.journal "read" {
-      forward_to     = [loki.write.local.receiver]
+      forward_to     = [loki.process.clean_journal.receiver]
       relabel_rules  = loki.relabel.journal.rules
       format_as_json = true
       labels         = { job = "systemd-journal", host = "${config.networking.hostName}" }
+    }
+
+    loki.process "clean_journal" {
+      forward_to = [loki.write.local.receiver]
+
+      stage.json {
+        expressions = {
+          message           = "MESSAGE",
+          priority          = "PRIORITY",
+          syslog_facility   = "SYSLOG_FACILITY",
+          syslog_identifier = "SYSLOG_IDENTIFIER",
+          cmdline           = "_CMDLINE",
+          comm              = "_COMM",
+          exe               = "_EXE",
+          gid               = "_GID",
+          pid               = "_PID",
+          systemd_unit      = "_SYSTEMD_UNIT",
+          transport         = "_TRANSPORT",
+          uid               = "_UID",
+        }
+      }
+
+      stage.template {
+        source   = "clean_json"
+        template = `{"message":{{ toJson .message }}{{ if .priority }},"level":"{{ if eq .priority "0" }}emergency{{ else if eq .priority "1" }}alert{{ else if eq .priority "2" }}critical{{ else if eq .priority "3" }}error{{ else if eq .priority "4" }}warning{{ else if eq .priority "5" }}notice{{ else if eq .priority "6" }}info{{ else if eq .priority "7" }}debug{{ else }}info{{ end }}"{{ end }}{{ if .syslog_facility }},"facility":"{{ if eq .syslog_facility "0" }}kern{{ else if eq .syslog_facility "1" }}user{{ else if eq .syslog_facility "2" }}mail{{ else if eq .syslog_facility "3" }}daemon{{ else if eq .syslog_facility "4" }}auth{{ else if eq .syslog_facility "5" }}syslog{{ else if eq .syslog_facility "6" }}lpr{{ else if eq .syslog_facility "7" }}news{{ else if eq .syslog_facility "8" }}uucp{{ else if eq .syslog_facility "9" }}cron{{ else if eq .syslog_facility "10" }}authpriv{{ else if eq .syslog_facility "11" }}ftp{{ else if eq .syslog_facility "12" }}ntp{{ else if eq .syslog_facility "13" }}security{{ else if eq .syslog_facility "14" }}console{{ else if eq .syslog_facility "15" }}clock{{ else if eq .syslog_facility "16" }}local0{{ else if eq .syslog_facility "17" }}local1{{ else if eq .syslog_facility "18" }}local2{{ else if eq .syslog_facility "19" }}local3{{ else if eq .syslog_facility "20" }}local4{{ else if eq .syslog_facility "21" }}local5{{ else if eq .syslog_facility "22" }}local6{{ else if eq .syslog_facility "23" }}local7{{ else }}unknown{{ end }}"{{ end }}{{ if .syslog_identifier }},"service":{{ toJson .syslog_identifier }}{{ end }}{{ if or .priority .syslog_facility .syslog_identifier .pid .uid .gid }},"syslog":{"present":true{{ if .priority }},"level":"{{ if eq .priority "0" }}emergency{{ else if eq .priority "1" }}alert{{ else if eq .priority "2" }}critical{{ else if eq .priority "3" }}error{{ else if eq .priority "4" }}warning{{ else if eq .priority "5" }}notice{{ else if eq .priority "6" }}info{{ else if eq .priority "7" }}debug{{ else }}info{{ end }}"{{ end }}{{ if .syslog_facility }},"facility":"{{ if eq .syslog_facility "0" }}kern{{ else if eq .syslog_facility "1" }}user{{ else if eq .syslog_facility "2" }}mail{{ else if eq .syslog_facility "3" }}daemon{{ else if eq .syslog_facility "4" }}auth{{ else if eq .syslog_facility "5" }}syslog{{ else if eq .syslog_facility "6" }}lpr{{ else if eq .syslog_facility "7" }}news{{ else if eq .syslog_facility "8" }}uucp{{ else if eq .syslog_facility "9" }}cron{{ else if eq .syslog_facility "10" }}authpriv{{ else if eq .syslog_facility "11" }}ftp{{ else if eq .syslog_facility "12" }}ntp{{ else if eq .syslog_facility "13" }}security{{ else if eq .syslog_facility "14" }}console{{ else if eq .syslog_facility "15" }}clock{{ else if eq .syslog_facility "16" }}local0{{ else if eq .syslog_facility "17" }}local1{{ else if eq .syslog_facility "18" }}local2{{ else if eq .syslog_facility "19" }}local3{{ else if eq .syslog_facility "20" }}local4{{ else if eq .syslog_facility "21" }}local5{{ else if eq .syslog_facility "22" }}local6{{ else if eq .syslog_facility "23" }}local7{{ else }}unknown{{ end }}"{{ end }}{{ if .syslog_identifier }},"identifier":{{ toJson .syslog_identifier }}{{ end }}{{ if .pid }},"pid":{{ toJson .pid }}{{ end }}{{ if .uid }},"uid":{{ toJson .uid }}{{ end }}{{ if .gid }},"gid":{{ toJson .gid }}{{ end }}}{{ end }}{{ if or .cmdline .comm .exe .systemd_unit .transport }},"process":{"present":true{{ if .cmdline }},"cmdline":{{ toJson .cmdline }}{{ end }}{{ if .comm }},"comm":{{ toJson .comm }}{{ end }}{{ if .exe }},"exe":{{ toJson .exe }}{{ end }}{{ if .systemd_unit }},"systemd_unit":{{ toJson .systemd_unit }}{{ end }}{{ if .transport }},"transport":{{ toJson .transport }}{{ end }}}{{ end }}}`
+      }
+      }
+
+      stage.output {
+        source = "clean_json"
+      }
     }
 
     local.file_match "postgres" {
