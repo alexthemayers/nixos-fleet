23550c4eea40835dd2dfd9b84c1b9cc7bb22140a
Alexander Mayers <a.mayers102@gmail.com>
2026-07-16 13:13:00 +0200

    chore(CI): Build and push devshell

--- config/system.nix
+++ config/system.nix
@@ -9,13 +9,11 @@
       substituters = [
         "http://proxmox-lb:8080/attic"
         "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
         "https://nixos-raspberrypi.cachix.org"
       ];
       trusted-public-keys = [
         "attic:4/oEWZvm70jexTDGnT/Xvv2wlV3cE4utycLPZUSbmAw="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
       download-buffer-size = 1073741824; # 1024 MiB
