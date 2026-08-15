5768796210a529d5a9a44e5c3add483df6116d4a
Alexander Mayers <a.mayers102@gmail.com>
2026-08-15 20:02:15 +0200

    Update hash

--- services/caddy-internal.nix
+++ services/caddy-internal.nix
@@ -29,7 +29,7 @@
         "github.com/mholt/caddy-l4@v0.1.1"
         "github.com/mholt/caddy-ratelimit@v0.1.1-0.20260612195517-5625512f24f6"
       ];
-      hash = "sha256-3lGfwEVaEH4Z+sa44Kqst+b+m0KhX8Aphd+8W6deEoA=";
+      hash = "sha256-Zo+LbslsZ80Ceijf25ZzVmSzWlEisi6EgGTvfxuN5fI=";
     };
 
     globalConfig = ''
