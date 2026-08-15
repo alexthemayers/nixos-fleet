49df036c6b3a9f7347ab5d8f158af880e9f0c448
Alexander Mayers <a.mayers102@gmail.com>
2026-08-15 14:18:37 +0200

    Update internal lb

--- services/caddy-internal.nix
+++ services/caddy-internal.nix
@@ -68,64 +68,18 @@
       "http://grafana.alexmayers.co.za" = {
         extraConfig = ''
           reverse_proxy proxmox-observability-1:3000 proxmox-observability-2:3000 {
-              lb_policy round_robin
+              lb_policy cookie grafana_lb
               health_uri /api/health
+              health_interval 5s
+              health_timeout 2s
+              health_status 200
               flush_interval -1
-              health_interval 5s
-              health_timeout 2s
-              health_status 200
-            }
-        '';
-      };
-      "http://mimir.alexmayers.co.za" = {
-        extraConfig = ''
-          reverse_proxy proxmox-observability-1:9009 proxmox-observability-2:9009 {
-              lb_policy round_robin
-              health_uri /ready
-              health_interval 5s
-              health_timeout 2s
-              health_status 200
-            }
-        '';
-      };
-      "http://loki.alexmayers.co.za" = {
-        extraConfig = ''
-          reverse_proxy proxmox-observability-1:3100 proxmox-observability-2:3100 {
-              lb_policy round_robin
-              health_uri /ready
-              health_interval 5s
-              health_timeout 2s
-              health_status 200
-            }
-        '';
-      };
-      "http://prometheus.alexmayers.co.za" = {
-        extraConfig = ''
-          reverse_proxy proxmox-observability-1:9090 proxmox-observability-2:9090 {
-              lb_policy round_robin
-              health_uri /-/healthy
-              health_interval 5s
-              health_timeout 2s
-              health_status 2xx
-            }
-        '';
-      };
-      "http://alertmanager.alexmayers.co.za" = {
-        extraConfig = ''
-          reverse_proxy proxmox-observability-1:9093 proxmox-observability-2:9093 {
-              lb_policy round_robin
-              health_uri /-/healthy
-              health_interval 5s
-              health_timeout 2s
-              health_status 2xx
             }
         '';
       };
       "http://gitlab.alexmayers.co.za" = {
         extraConfig = ''
-          reverse_proxy proxmox-applications-2:8080 {
-              flush_interval -1
-            }
+          reverse_proxy proxmox-applications-2:8080
         '';
       };
       "http://registry.alexmayers.co.za" = {
@@ -135,9 +89,7 @@
       };
       "http://coder.alexmayers.co.za" = {
         extraConfig = ''
-          reverse_proxy proxmox-dev:7080 {
-              flush_interval -1
-            }
+          reverse_proxy proxmox-dev:7080
         '';
       };
       "http://budget.alexmayers.co.za" = {
@@ -149,6 +101,10 @@
         extraConfig = ''
           reverse_proxy proxmox-applications-1:28981 proxmox-applications-2:28981 {
               lb_policy round_robin
+              lb_try_duration 5s
+              fail_duration 30s
+              max_fails 1
+              unhealthy_status 5xx
             }
         '';
       };
@@ -180,25 +136,7 @@
               fail_duration 10s
               max_fails 1
               unhealthy_status 5xx
-            }
-        '';
-      };
-      "http://s3.alexmayers.co.za" = {
-        extraConfig = ''
-          reverse_proxy /health proxmox-db-1:3903 proxmox-db-2:3903 {
-              lb_policy round_robin
-            }
-          reverse_proxy proxmox-db-1:3902 proxmox-db-2:3902 {
-              lb_policy round_robin
-              lb_try_duration 5s
-              health_uri /health
-              health_port 3903
-              health_interval 5s
-              health_timeout 2s
-              health_status 200
-              fail_duration 10s
-              max_fails 1
-              unhealthy_status 5xx
+              flush_interval -1
             }
         '';
       };
